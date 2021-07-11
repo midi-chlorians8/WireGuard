0a69d06e0901d37e869787166f62a3bf5d4e86d3
midi-chlorians8 <50805334+midi-chlorians8@users.noreply.github.com>
2021-07-11 11:59:23 +0300

    Add files via upload

---
+++ Wireguard-install-modify-from-terraform.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Downloan wireguard and make file executed
+curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+chmod +x wireguard-install.sh
